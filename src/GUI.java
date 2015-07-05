4e162003431fc6b8892860516ab8f7b6d6f2e1c7
DavidBarishev <david7815@gmail.com>
2015-07-05 23:08:50 +0300

    GUI

--- src/GUI.java
+++ src/GUI.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 
 public class GUI extends JFrame {
@@ -34,8 +35,12 @@ public class GUI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(48, 32, 304, 173);
+		contentPane.add(btnNewButton);
 	}
 
 }
