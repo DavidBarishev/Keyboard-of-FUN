06b9bf3025034daebb7158fcd70671f6f6d8c9dd
barr520 <barr520@gmail.com>
2015-07-13 13:49:50 +0300

    fixes to creator

--- src/creator.java
+++ src/creator.java
@@ -3,30 +3,31 @@ import java.io.IOException;
 //Creating a profile according to what the GUI sends and return s the data with a name at the end
 public class creator {
 	private letter[] letters;
-	//private int timesPressed;
-	//private int avgIdleTime;
-	//private int lastRelease;
+
+	// private int timesPressed;
+	// private int avgIdleTime;
+	// private int lastRelease;
 
 	creator() {
-		//avgIdleTime = 0;
-		//timesPressed = 0;
-		//lastRelease = 0;
-		for (int i = (int) 'a'; i <= (int) 'z'; i++) {
-			letters[i - 'a'] = new letter((char) i);
+		// avgIdleTime = 0;
+		// timesPressed = 0;
+		// lastRelease = 0;
+		letters = new letter[24];
+		for (int i = 0; i <letters.length; i++) {
+			letters[i] = new letter((char)(i+(int)'a'));
 		}
 	}
 
 	public void add(char c, int pressTime) {
-		letters[(int) c].add(pressTime);
-		/*if (lastRelease != 0) {
-			avgIdleTime = (avgIdleTime * timesPressed + pressTime)
-					/ (timesPressed + 1);
-		}
-		lastRelease = release;*/
-		//timesPressed++;
+		letters[(int)c-'a'].add(pressTime);
+		/*
+		 * if (lastRelease != 0) { avgIdleTime = (avgIdleTime * timesPressed +
+		 * pressTime) / (timesPressed + 1); } lastRelease = release;
+		 */
+		// timesPressed++;
 	}
 
-	public void finish(String name)  {
+	public void finish(String name) {
 		profile profile = new profile(name, letters);
 		try {
 			profile.output();
