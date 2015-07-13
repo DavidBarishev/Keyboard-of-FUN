40a786a9e97b2685f5794ac73ab263c16840f946
NotGivingShi* <yonatan1479@gmail.com>
2015-07-13 12:47:50 +0300

    Profiler // Yonatan
    L   O  O   K

---
+++ src/Profiler.java
@@ -0,0 +1,15 @@
+//this class checks if 2 profiles are the same.
+//or NOT.
+public class Profiler{
+	
+	profile user;
+	profile[] compareTo;
+	
+	public boolean comapare(){
+		
+		
+		
+		return false;
+	}
+
+}
