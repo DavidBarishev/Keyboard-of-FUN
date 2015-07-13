dc567e70cfe8406bff1906aa8fe2ebfec8dda383
NotGivingShi* <yonatan1479@gmail.com>
2015-07-13 14:28:27 +0300

    Not 100% working // Yonatan

--- src/Profiler.java
+++ src/Profiler.java
@@ -2,13 +2,24 @@
 //or NOT.
 public class Profiler{
 	
-	public static profile compare(profile[] compareTo, profile user){
-		
-		for(int i = 0; i < compareTo.length; i++){
-			
-		}
-		
-		return user;
-	}
+	public static profile compare(profile[] users, profile comparable){
 
+		int[] sum = new int[2];
+		int currentSum = 0;
+		
+		for(int i = 0; i < users.length; i++){
+			for(int j = 0; j < comparable.letters.length; j++){
+				currentSum = currentSum + comparable.letters[j].avgPressTime / users[i].letters[j].avgPressTime;
+			}
+			if(sum[0] == 0){
+				sum[0] = currentSum;
+				sum[1] = i;
+			}if(currentSum < sum[0]){
+				sum[0] = currentSum;
+				sum[1] = i;
+			}
+		}
+		return users[sum[1]];
+	}
+	
 }
