b89b96459bd043a2596670f8367f0b2f31e33699
barr520 <barr520@gmail.com>
2015-09-04 17:56:35 +0300

    rewritten the profiling class
    needs to be tested, but isn't called from the GUI yet.

--- src/Profiler.java
+++ src/Profiler.java
@@ -1,13 +1,13 @@
 //this class checks if 2 profiles are the same.
 //or NOT.
 public class Profiler{
-	
-	public static profile compare(profile[] users, profile comparable){
+	/*public static profile compare(profile[] users, profile comparable){
 
 		int[] sum = new int[2];
 		int currentSum = 0;
 		
 		for(int i = 0; i < users.length; i++){
+		 
 			for(int j = 0; j < comparable.letters.length; j++){
 				currentSum = currentSum + comparable.letters[j].avgPressTime / users[i].letters[j].avgPressTime;
 			}
@@ -20,6 +20,44 @@ public class Profiler{
 			}
 		}
 		return users[sum[1]];
+	}*/
+	public static profile compare(profile[] users, profile comparable,int threshold){
+		float[] proximity = new float[users.length];
+		for(int i=0;i<users.length;i++){
+			proximity[i]=approximate(users[i], comparable);
+		}
+		int bestOption = bestOption(proximity, threshold);
+		if(bestOption==-1){
+			return null;
+		}
+		else{
+			return users[bestOption];
+		}
 	}
-	
+	private static int bestOption(float[] proximity,int threshold) {
+		float best = -1;
+		int bestProximity=-1;
+		for(int i =0;i<proximity.length;i++){
+			if(proximity[i]>=threshold&&proximity[i]>best){
+				best=proximity[i];
+				bestProximity=i;
+			}
+		}
+		return bestProximity;
+	}
+	public static float approximate(profile user, profile comparable){
+		int[] letterProximity = new int[24];
+		for(int i = 0;i<23;i++){
+			letterProximity[i]=Math.abs(user.letters[i].avgPressTime-comparable.letters[i].avgPressTime);
+		}
+		return avgProximity(letterProximity);
+	}
+	private static float avgProximity(int[] letterProximity) {
+		int sum=0;
+		for(int i=0;i<letterProximity.length;i++){
+			sum+=letterProximity[i];
+		}
+		return sum/letterProximity.length;
+	}
+
 }
