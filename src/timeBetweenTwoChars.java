b6e8218be2e0bf33fd3953b6db554945c78f211c
David Barishev <david7815@gmail.com>
2015-10-27 21:09:03 +0200

    New class to hold time between pair of chars

---
+++ src/timeBetweenTwoChars.java
@@ -0,0 +1,38 @@
+public class timeBetweenTwoChars {
+	private char first;
+	private char second;
+	private int avgIdleTime;
+
+	private int timesPressed = 0;
+
+	public timeBetweenTwoChars(char first, char second, int avgIdleTime) {
+		this.first = first;
+		this.second = second;
+		this.avgIdleTime = avgIdleTime;
+	}
+
+	public void addIdleTime(int idleTime) {
+		this.avgIdleTime = ((this.avgIdleTime * this.timesPressed) + idleTime) / (this.timesPressed + 1);
+		this.timesPressed++;
+	}
+
+	public boolean isEqual(timeBetweenTwoChars other) {
+		if (other.first == this.first && other.second == this.second) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int getAvgIdleTime(){
+		return this.avgIdleTime;
+	}
+	
+	public char getFirstLetter(){
+		return this.first;
+	}
+	
+	public char getSecondLetter(){
+		return this.second;
+	}
+
+}
