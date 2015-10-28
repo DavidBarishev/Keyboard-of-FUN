63a70744cd587c27bd518410fc5be19187f47b02
roeiyosef <roei.a.yosef@gmail.com>
2015-10-28 21:13:33 +0200

    few more tests

--- src/Tests.java
+++ src/Tests.java
@@ -13,48 +13,48 @@ public class Tests {
 		letter x = new letter();
 
 		Assert.assertNotNull("letter should not be null", x);
-		
+
 	}
 
 	@Test
-	public void letter_SecondConstructorNotNull(){
-	     letter x = new letter(1);
-	     Assert.assertNotNull("letter should not be null",x);
-	     
-	     
+	public void letter_SecondConstructorNotNull() {
+		letter x = new letter(1);
+		Assert.assertNotNull("letter should not be null", x);
+
 	}
+
 	@Test
-	public void letter_add(){
-	     letter x = new letter(2);
-	     int y= x.getTimePressed();
-	     x.add(1);
-	     int z= x.getTimePressed();
-	     Assert.assertEquals(z,y+1);  
+	public void letter_add() {
+		letter x = new letter(2);
+		int y = x.getTimePressed();
+		x.add(1);
+		int z = x.getTimePressed();
+		Assert.assertEquals(z, y + 1);
 	}
-	
+
 	@Test
-	 public void profile_ConstructorNotNull(){
-		letter [] array= new letter[2];
+	public void profile_ConstructorNotNull() {
+		letter[] array = new letter[2];
 		letter x = new letter(3);
 		letter y = new letter(2);
-		array[0]=x;
-		array[1]=y;
+		array[0] = x;
+		array[1] = y;
 		ArrayList<timeBetweenTwoChars> d = new ArrayList<timeBetweenTwoChars>();
-	     profile test=new profile("profile1",array,d);
-	     Assert.assertNotNull(test);
+		profile test = new profile("profile1", array, d);
+		Assert.assertNotNull(test);
 	}
+
 	@Test
-	 public void profile_SecondConstructorNotNull(){
-			File testfile= new File("test");
-			profile test= new profile(testfile);
-			Assert.assertNotNull(test);
+	public void profile_SecondConstructorNotNull() {
+		File testfile = new File("test");
+		profile test = new profile(testfile);
+		Assert.assertNotNull(test);
+	}
+
+	@Test
+	 public void creator_ConstructorNotNull(){
+			 creator x = new creator();
+			 Assert.assertNotNull(x);
 	 }
-	
-	
-	
-	
-	
-	
-	
 	
 }
