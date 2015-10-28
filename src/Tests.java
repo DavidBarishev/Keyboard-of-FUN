9d9878be8b2db5a962df2a8350e8f3528db77799
roeiyosef <roei.a.yosef@gmail.com>
2015-10-28 17:51:26 +0200

    added library and first test

---
+++ src/Tests.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import org.junit.Assert;
+public class Tests {
+  /* 
+   * testing for: letter  
+   */
+	@Test
+	public void letter_ConstructorNotNull(){
+		letter x= new letter();
+		
+		Assert.assertNotNull(x);
+		
+	    
+	}
+}
