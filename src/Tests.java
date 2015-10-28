1256e00ab43359ac1a89b51604d369024d490ee7
roeiyosef <roei.a.yosef@gmail.com>
2015-10-28 18:13:30 +0200

    fixed library path

--- src/Tests.java
+++ src/Tests.java
@@ -1,15 +1,21 @@
 import org.junit.Test;
 import org.junit.Assert;
+
 public class Tests {
-  /* 
-   * testing for: letter  
-   */
+	/*
+	 * testing for: letter
+	 */
 	@Test
-	public void letter_ConstructorNotNull(){
-		letter x= new letter();
-		
-		Assert.assertNotNull(x);
-		
-	    
+	public void letter_ConstructorNotNull() {
+		letter x = new letter();
+
+		Assert.assertNotNull("letter should not be null", x);
+	}
+
+	@Test
+	public void letter_SecondConstructorNotNull(){
+		letter x = new letter(1);
+	     Assert.assertNotNull("letter should not be null",x);
+	     
 	}
 }
