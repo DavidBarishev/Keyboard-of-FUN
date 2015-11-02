cf7408582408df9c75da3e8be55bb747ab221387
DavidBarishev <david7815@gmail.com>
2015-11-02 12:05:32 +0200

    Added statistical information (one/two hands ) NEED TO TINKER WITH MAX IDLE TO FROM ONE HAND TO TWO HANDS

---
+++ src/StatisticalInfo.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class StatisticalInfo {
+	private ArrayList<timeBetweenTwoChars> pairsList;
+	
+	private boolean twoHands;
+	private boolean oneHand;
+	
+	private char[] leftKeyboard = {'q','w','e','a','s','d','z','x','c'};
+	private char[] rightKeyboard = {'i','o','p','k','l','n','m'};
+	
+	private ArrayList<timeBetweenTwoChars> longKeyboardPairs = new ArrayList<timeBetweenTwoChars>();
+	private ArrayList<timeBetweenTwoChars> rightSideOfKeyboard = new ArrayList<timeBetweenTwoChars>();
+	
+	private final int MAX_IDEL_TIME = 300;
+	
+	public StatisticalInfo(ArrayList<timeBetweenTwoChars> pairsList){
+		this.pairsList = pairsList;
+		this.setNumberOfHands();
+	}
+	
+	private void getAllLongPairs(){
+		for(timeBetweenTwoChars elemnt: this.pairsList){
+			if((contains(leftKeyboard, elemnt.getFirstLetter()) && contains(rightKeyboard, elemnt.getSecondLetter()))||
+					(contains(rightKeyboard, elemnt.getFirstLetter()) && contains(leftKeyboard, elemnt.getSecondLetter()))){
+				this.longKeyboardPairs.add(elemnt);
+			}
+		}
+	}
+	
+	private void setNumberOfHands(){
+		getAllLongPairs();
+		if(getAverageTimeOfLongPairs() > MAX_IDEL_TIME){
+			this.oneHand = true;
+			this.twoHands =false;
+		}
+		else{
+			this.oneHand = false;
+			this.twoHands = true;
+		}	
+		
+		
+		
+	}
+	
+	private double getAverageTimeOfLongPairs(){
+		int sum = 0;
+		for(timeBetweenTwoChars elemnt : this.longKeyboardPairs){
+			sum+= elemnt.getAvgIdleTime();
+		}
+		return sum / this.longKeyboardPairs.size();
+	}
+	
+	private static boolean contains(char[] partOfKeyboard,char value){
+		for(char elemnt : partOfKeyboard){
+			if(elemnt == value){
+				return true;
+			}
+		}
+		return false;
+	}
+}
