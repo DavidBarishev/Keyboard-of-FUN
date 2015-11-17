64adbf8f073c04bee045731b28bc1638e7191b6a
David Barishev <david7815@gmail.com>
2015-11-17 21:52:19 +0200

    Added framwork

--- src/FixedProfiler.java
+++ src/FixedProfiler.java
@@ -1,9 +1,5 @@
 /**
- * @author User
- *
- */
-/**
- * @author User
+ * @author david
  *
  */
 public class FixedProfiler {
@@ -24,7 +20,38 @@ public class FixedProfiler {
 	}
 	
 	
+	/**
+	 * @param profilePool - profile to find the profile from (Valid values)
+	 * @param profileToFind - the profile to find best match to (Valid values)
+	 * @return best matching profile
+	 */
 	private static profile getBestMatch_INTERNAL(profile[] profilePool , profile profileToFind){
-		return null;
+		double[] gradesForEachProfile =  new double[profilePool.length];
+		
+		for(int i = 0 ; i < gradesForEachProfile.length ; i++){
+			gradesForEachProfile[i] = getGradeForProfile(profilePool[i],profileToFind);
+		}
+
+		return getBestProfileByGrade(profilePool,profileToFind,gradesForEachProfile);
+	}
+
+
+	private static profile getBestProfileByGrade(profile[] profilePool, profile profileToFind,
+			double[] gradesForEachProfile) {
+		double max = 0;
+		int index  = 0;
+		for(int i = 0 ; i < gradesForEachProfile.length ; i++){
+			if(gradesForEachProfile[i] > max){
+				max = gradesForEachProfile[i];
+				index  = i;
+			}
+		}
+		return profilePool[index];
+	}
+
+
+	private static double getGradeForProfile(profile profile, profile profileToFind) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
